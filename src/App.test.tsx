8bb28dd997ce69bc2f2f7fede642656c1eac60da
Aaron Mast <amast09@gmail.com>
2020-09-28 19:21:52 -0400

    The CI, it works

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "./App";
 
-test('the weather app renders the correct title', () => {
+test("the weather app renders the correct title", () => {
   const { getByText } = render(<App />);
 
   const weatherAppTitle = getByText(/weather app/i);
