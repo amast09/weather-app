2b5f767e1a891fc643bd924547b6b53fcd94cf50
Aaron Mast <amast09@gmail.com>
2020-10-08 19:09:13 -0400

    Implement API interface for retrieving data from open weather API

---
+++ src/api/externalApis/openWeatherApi.test.ts
@@ -0,0 +1,164 @@
+/**
+ * @jest-environment node
+ */
+import axios from "axios";
+import nock from "nock";
+import {
+  CityQuery,
+  CityStateQuery,
+  LatitudeLongitudeQuery,
+  QueryType,
+  ZipCodeQuery,
+} from "../helpers/locationStringToQuery";
+import openWeatherApi from "./openWeatherApi";
+import { CurrentWeatherConditions } from "../../shared/types/OpenWeatherResponses";
+
+const OPEN_WEATHER_API_BASE_URL = "https://api.openweathermap.org";
+const fakeApiKey = "gibberish";
+const fakeSuccessfulResponse: CurrentWeatherConditions = {
+  coord: { lon: 139, lat: 35 },
+  weather: [
+    {
+      id: 800,
+      main: "Clear",
+      description: "clear sky",
+      icon: "01n",
+    },
+  ],
+  base: "stations",
+  main: {
+    temp: 281.52,
+    feels_like: 278.99,
+    temp_min: 280.15,
+    temp_max: 283.71,
+    pressure: 1016,
+    humidity: 93,
+  },
+  wind: {
+    speed: 0.47,
+    deg: 107.538,
+  },
+  clouds: {
+    all: 2,
+  },
+  dt: 1560350192,
+  sys: {
+    type: 3,
+    id: 2019346,
+    message: 0.0065,
+    country: "JP",
+    sunrise: 1560281377,
+    sunset: 1560333478,
+  },
+  timezone: 32400,
+  id: 1851632,
+  name: "Shuzenji",
+  cod: 200,
+};
+
+axios.defaults.baseURL = OPEN_WEATHER_API_BASE_URL;
+
+describe("openWeatherApi", () => {
+  describe("getCurrentWeather", () => {
+    it("returns a void promise when the Open Weather API request fails", async () => {
+      const query: ZipCodeQuery = {
+        queryType: QueryType.ZipCode,
+        zipCode: "29464",
+      };
+
+      nock(OPEN_WEATHER_API_BASE_URL)
+        .get("/data/2.5/weather")
+        .query({
+          zipcode: query.zipCode,
+          appid: fakeApiKey,
+        })
+        .reply(400);
+
+      await expect(
+        openWeatherApi.getCurrentWeather(fakeApiKey, query)
+      ).rejects.toThrow();
+    });
+
+    describe("when the Open Weather API responds successfully", () => {
+      it("returns the response when requesting for weather for a zipcode", async () => {
+        const query: ZipCodeQuery = {
+          queryType: QueryType.ZipCode,
+          zipCode: "29464",
+        };
+
+        nock(OPEN_WEATHER_API_BASE_URL)
+          .get("/data/2.5/weather")
+          .query({
+            zipCode: query.zipCode,
+            appid: fakeApiKey,
+          })
+          .reply(200, fakeSuccessfulResponse);
+
+        await expect(
+          openWeatherApi.getCurrentWeather(fakeApiKey, query)
+        ).resolves.toEqual(fakeSuccessfulResponse);
+      });
+
+      it("returns the response when requesting for weather for a latitude and longitude", async () => {
+        const query: LatitudeLongitudeQuery = {
+          queryType: QueryType.LatitudeLongitude,
+          latitude: 0,
+          longitude: 1,
+        };
+
+        nock(OPEN_WEATHER_API_BASE_URL)
+          .get("/data/2.5/weather")
+          .query({
+            lat: query.latitude,
+            lon: query.longitude,
+            appid: fakeApiKey,
+          })
+          .reply(200, fakeSuccessfulResponse);
+
+        await expect(
+          openWeatherApi.getCurrentWeather(fakeApiKey, query)
+        ).resolves.toEqual(fakeSuccessfulResponse);
+      });
+
+      it("returns the response when requesting for weather for a city and state", async () => {
+        const query: CityStateQuery = {
+          queryType: QueryType.CityState,
+          city: "Clemson",
+          state: "South Carolina",
+        };
+
+        nock(OPEN_WEATHER_API_BASE_URL)
+          .get("/data/2.5/weather")
+          .query({
+            city: query.city,
+            state: query.state,
+            appid: fakeApiKey,
+          })
+          .reply(200, fakeSuccessfulResponse);
+
+        await expect(
+          openWeatherApi.getCurrentWeather(fakeApiKey, query)
+        ).resolves.toEqual(fakeSuccessfulResponse);
+      });
+
+      it("returns the response when requesting for weather for a city", async () => {
+        const query: CityQuery = {
+          queryType: QueryType.City,
+          city: "Clemson",
+        };
+
+        nock(OPEN_WEATHER_API_BASE_URL)
+          .get("/data/2.5/weather")
+          .query({
+            city: query.city,
+            appid: fakeApiKey,
+          })
+          .reply(200, fakeSuccessfulResponse);
+
+        await expect(
+          openWeatherApi.getCurrentWeather(fakeApiKey, query)
+        ).resolves.toEqual(fakeSuccessfulResponse);
+      });
+    });
+  });
+});
---
+++ src/api/externalApis/openWeatherApi.ts
@@ -0,0 +1,48 @@
+import { LocationQuery, QueryType } from "../helpers/locationStringToQuery";
+import { CurrentWeatherConditions } from "../../shared/types/OpenWeatherResponses";
+import axios from "axios";
+import Dictionary from "../../shared/types/Dictionary";
+
+const OPEN_WEATHER_API_BASE_URL = "https://api.openweathermap.org";
+
+const getRequestData = (query: LocationQuery): Dictionary<string> => {
+  switch (query.queryType) {
+    case QueryType.ZipCode:
+      return {
+        zipCode: query.zipCode,
+      };
+    case QueryType.LatitudeLongitude:
+      return {
+        lat: query.latitude.toString(10),
+        lon: query.longitude.toString(10),
+      };
+    case QueryType.City:
+      return {
+        city: query.city,
+      };
+    case QueryType.CityState:
+      return {
+        city: query.city,
+        state: query.state,
+      };
+  }
+};
+
+const getCurrentWeather = (
+  apiKey: string,
+  locationQuery: LocationQuery
+): Promise<CurrentWeatherConditions> => {
+  return axios
+    .get<CurrentWeatherConditions>(
+      `${OPEN_WEATHER_API_BASE_URL}/data/2.5/weather`,
+      {
+        params: {
+          ...getRequestData(locationQuery),
+          appid: apiKey,
+        },
+      }
+    )
+    .then((result) => result.data);
+};
+
+export default { getCurrentWeather };
